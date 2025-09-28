f36e594da72f63953e287759fd73063619facdb3
Anna Nemtseva <anevnemtseva@edu.hse.ru>
2025-09-28 09:52:03 +0300

    Изменен файл

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 
 print(f"Вы ввели: {num1} и {num2}")
+
+result = num1 + num2
+print(f"Сумма чисел: {result}")
