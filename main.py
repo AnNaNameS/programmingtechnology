080e0fcaa4fe3f62e9fbc41959b053dc417dd617
AnNaNameS <annanemtc3va@yandex.ru>
2025-09-28 09:52:55 +0300

    Изменен файл

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ num2 = int(input("Введите второе число: "))
 
 print(f"Вы ввели: {num1} и {num2}")
 
-result = num1 + num2
-print(f"Сумма чисел: {result}")
+result = num1 - num2
+print(f"Разность чисел: {result}")
